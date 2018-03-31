0f13b5d59bdc9652bebfd0fd61eb9138d2d437f3
leandro.garcia <leandro.garcia@globant.com>
2018-03-30 22:04:53 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {Provider} from 'react-redux';
+
+import store from './store';
+import Home from './Home';
+
+export default () => (
+  <Provider store={store}>
+    <Home />
+  </Provider>
+);
+
+
